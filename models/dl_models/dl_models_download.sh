ce7701d9fcfb1d29feccfa607822d0d0c76d1eed
Bob Adolf <rdadolf@gmail.com>
2018-01-21 22:40:49 +0000

    Added download scripts for external models.

---
+++ models/dl_models/dl_models_download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Note: until dl_models is released on github, you must have a login to a
+# Harvard ACC machine to download these.
+GITHOST=harvardacc
+git clone $GITHOST:/group/vlsiarch/git/dl-models
