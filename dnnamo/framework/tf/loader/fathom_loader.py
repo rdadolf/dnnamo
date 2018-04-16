471e03ecc848dab3f6a2e0b0022430d48606fb1e
Bob Adolf <rdadolf@gmail.com>
2018-04-16 20:52:31 +0000

    Fixes to Fathom loader to support profiling.

--- dnnamo/framework/tf/loader/fathom_loader.py
+++ dnnamo/framework/tf/loader/fathom_loader.py
@@ -3,6 +3,7 @@ import sys
 
 from ....core.model import DnnamoModel
 from ....core.loader import BaseLoader
+from ..tf_model import session_run, SessionProfiler
 
 FATHOM_MODELS = [
   'Seq2Seq',
@@ -16,13 +17,25 @@ FATHOM_MODELS = [
 ]
 
 class FathomModel(DnnamoModel):
-  def __init__(self, ModelClass):
-    self._fathommodel = ModelClass()
-  def get_graph(self):
-    return self._fathommodel.G
+  def __init__(self, ModelClass, ModelClassFwd):
+    self._fathommodel_train = ModelClass()
+    self._fathommodel_inf = ModelClassFwd()
+    self._fathommodel_train.setup()
+    self._fathommodel_inf.setup()
+
+  def get_training_graph(self):
+    return self._fathommodel_train.G
+
+  def get_inference_graph(self):
+    return self._fathommodel_inf.G
+
   def get_weights(self, keys=None):
     raise NotImplementedError, 'Fathom does not have a standard weights interface.'
 
+  def profile_training(self, n_steps=1, *args, **kwargs):
+    profiler = SessionProfiler()
+    self._fathommodel_train.run(runstep=profiler.session_profile, n_steps=n_steps)
+    return profiler.rmd
 
 class TFFathomLoader(BaseLoader):
   def __init__(self, identifier, fathompath=None):
@@ -51,7 +64,11 @@ class TFFathomLoader(BaseLoader):
       ModelClass = getattr(module, self.identifier)
     except KeyError:
       raise NameError, 'No '+str(self.identifier)+' class found in Fathom.'
+    try:
+      ModelClassFwd = getattr(module, self.identifier+'Fwd')
+    except KeyError:
+      raise NameError, 'No '+str(self.identifier)+'Fwd class found in Fathom.'
 
-    m = FathomModel(ModelClass)
+    m = FathomModel(ModelClass, ModelClassFwd)
 
     return m
--- dnnamo/framework/tf/tf_model.py
+++ dnnamo/framework/tf/tf_model.py
@@ -16,13 +16,41 @@ def session_run(session, fetches, feed_dict=None):
   to tf.Session.run().'''
   return session.run(fetches, feed_dict, options=None, run_metadata=None)
 
+
 def session_profile(session, fetches, feed_dict=None):
-  '''Replacement for tf.Session.run(...) which returns profiling data.
+  # NOTE: If you use this function, you must manually aggregate multiple
+  # steps. I.e., the model interface method "profile_training" expects a
+  # list of rmd structures, so you must manually combine them in this
+  # function's caller.
+
+  '''Partial replacement for tf.Session.run(...) which returns profiling data.
 
   The first argument must be a tf.Session().
   Note that this function does not accept the options or run_metadata arguments
-  to tf.Session.run().'''
+  to tf.Session.run(). Additionally, this function returns runmetadata, not
+  the normal output of Session.run(), so if your model needs it, use the
+  stateful helper class SessionProfiler.'''
+
   opts = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE,output_partition_graphs=True)
   rmd = tf.RunMetadata()
-  session.run(fetches, feed_dict, options=opts, run_metadata=rmd)
+  _ = session.run(fetches, feed_dict, options=opts, run_metadata=rmd)
   return rmd
+
+###
+
+class SessionProfiler(object):
+  '''A stateful wrapper for tf.Session.run. When used, profiling data is recorded in the .rmd attribute.'''
+
+  def __init__(self):
+    self.rmd = []
+  
+  def session_profile(self, session, fetches, feed_dict=None):
+    '''Replacement for tf.Session.run(...) which returns profiling data.
+
+    The first argument must be a tf.Session().
+    Note that this function does not accept the options or run_metadata arguments
+    to tf.Session.run().'''
+    opts = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE,output_partition_graphs=True)
+    self.rmd.append(tf.RunMetadata())
+    out = session.run(fetches, feed_dict, options=opts, run_metadata=self.rmd[-1])
+    return out
