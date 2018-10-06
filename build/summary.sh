b737607409bb890fa44f27a330da0fb402bba9d4
Bob Adolf <rdadolf@gmail.com>
2018-10-06 19:02:48 +0000

    Cleaned up test runners to allow a bit more flexibility.
    The external (read: Fathom/FathomLite) tests take a bit longer than the normal
      tests, which was slowing me down. Most of the time, these aren't all that
      important (or rather, they aren't what I'm actually looking for).
    Using -a or --all with test.sh will run everything including the external tests.
    Using -s or --summary will give the old summary.sh behavior.
    Since test.sh now does both, we don't need summary.sh anymore. Removed it.

--- build/summary.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-pytest --no-print-logs --ignore=models --tb=no $@
--- build/test.sh
+++ build/test.sh
@@ -1,2 +1,31 @@
 #!/bin/bash
-pytest --ignore=models $@
+FLAGS_SKIP="-k 'not external'"
+FLAGS_SUMMARY=""
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+  key="$1"
+  case $key in
+      -a|--all)
+      FLAGS_SKIP=""
+      shift
+      ;;
+      -s|--summary)
+      FLAGS_SUMMARY="--tb=no --no-print-logs"
+      shift
+      ;;
+      -*) # Unknown flag
+      echo "Unrecognized flag \"$key\""
+      shift
+      ;;
+      *) # Positional arguments
+      POSITIONAL+=("$1") # save it in an array for later
+      shift # past argument
+      ;;
+  esac
+done
+
+cmd="pytest --ignore=models $FLAGS_SKIP $FLAGS_SUMMARY $POSITIONAL"
+echo "$cmd"
+eval $cmd
